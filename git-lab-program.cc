398e31586140bab727689dbb5edbfe434323504d
A.J. Prior <ap884921@ohio.edu>
2023-01-25 17:17:44 -0500

    Program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: A.J. Prior
+ *        Date: January 25, 2023
+ * Description: Simple Hello program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
